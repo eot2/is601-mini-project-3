d78e95890995042f929956ae25c80bb3408c24ad
eot2 <eot2@njit.edu>
2018-07-07 11:52:29 -0400

    Wrote a feature test to check if the about page returns a 200 status code

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -26,4 +26,11 @@ class FeatureTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testAboutPage()
+    {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+    }
 }
