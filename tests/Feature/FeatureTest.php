0b6d3d382c352c9056a9e3e83f3523e1c7c8f5ed
eot2 <eot2@njit.edu>
2018-07-07 11:51:40 -0400

    Wrote a feature test to check if the login page returns a 200 status code

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRegisterPage()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function testLoginPage()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
